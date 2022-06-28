d4ae2f8ddf44942e010280029cf6c180b4ff9713
Mike Dougherty <miked@threedev.com>
2022-06-27 22:37:50 -0700

    solve AlienCodex challenge with help
    Borrow the solution from cmichel.io's Github account.
    
    Relates to TDB-553

--- hardhat.config.js
+++ hardhat.config.js
@@ -47,6 +47,9 @@ module.exports = {
                 version: "0.6.12",
             },
             {
+                version: "0.5.0",
+            },
+            {
                 version: "0.4.24",
             },
         ],

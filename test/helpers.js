f6ca79b11b2cccd92759fa39c52e459ee51bec33
Mike Dougherty <miked@threedev.com>
2022-06-30 12:37:01 -0700

    include common helpers functions in a shared file
    Relates to TDB-553

---
+++ test/helpers.js
@@ -0,0 +1,15 @@
+const { ethers, network } = require("hardhat")
+
+module.exports.getContract = async (contractName, contractAddress) => {
+    console.log(`test/helpers: getContract(${contractName}, ${contractAddress})`);
+    const factory = await ethers.getContractFactory(contractName);
+    let contract;
+    if (contractAddress) {
+        console.log(`test/helpers: Getting ${contractName} at ${contractAddress} on ${network.name}...`);
+        contract = await factory.attach(contractAddress);
+    } else {
+        console.log(`test/helpers: Deploying ${contractName} on ${network.name}...`);
+        contract = await factory.deploy();
+    }
+    return contract;
+};

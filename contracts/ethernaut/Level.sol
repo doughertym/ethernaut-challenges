f13dcc6857e9ecbd4a85480e5433ea27e72d7546
Mike Dougherty <miked@threedev.com>
2022-07-03 21:16:24 -0700

    include LevelCompleted event
    Relates to TDB-553

--- contracts/ethernaut/Level.sol
+++ contracts/ethernaut/Level.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.6.0;
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 abstract contract Level is Ownable {
+    event LevelInstanceCreatedLog(address indexed player, address instance);
+    event LevelCompletedLog(address indexed player, address instance);
+
     function createInstance(address _player) virtual public payable returns (address);
     function validateInstance(address payable _instance, address _player) virtual public returns (bool);
 }
